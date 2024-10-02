0aebadcc8b989fc3fbddbe4b19236ea8e82ef940
predatorsense03 <janglobio@gmail.com>
2024-10-03 06:33:57 +0800

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "test"
+    return render_template("index.html")
 
 
 if __name__ in "__main__":
